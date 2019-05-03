fecadeda65555a1790b3ab6894780fd228c4ff94
김현준 <hyunjoon@gimhyeonjun-ui-MacBookPro.local>
2019-05-03 11:49:56 +0900

    Board.js에 상태 추가 후 renderSquare에 상태에 따라 내용을 보여주도록 작성

--- src/components/Board.js
+++ src/components/Board.js
@@ -2,8 +2,21 @@ import React, { Component } from 'react'
 import Square from './Square'
 
 export default class Board extends Component {
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         squares: Array(9).fill(null)
+      }
+    }
+
+  
+    
+
     renderSquare(i) {
-        return <Square value={i} />
+        return <Square value={this.state.squares[i]} 
+        onClick={() => this.handleClick(i)}
+        />
       }
     
       render() {
@@ -14,22 +27,21 @@ export default class Board extends Component {
             <div className="status">{status}</div>
             
             <div className="board-row">
-              {this.renderSquare(0)}
-              {this.renderSquare(1)}
-              {this.renderSquare(2)}
+                {this.renderSquare(0)}
+                {this.renderSquare(1)}
+                {this.renderSquare(2)}
             </div>
             <div className="board-row">
-              {this.renderSquare(3)}
-              {this.renderSquare(4)}
-              {this.renderSquare(5)}
+                {this.renderSquare(3)}
+                {this.renderSquare(4)}
+                {this.renderSquare(5)}
             </div>
             <div className="board-row">
-              {this.renderSquare(6)}
-              {this.renderSquare(7)}
-              {this.renderSquare(8)}
+                {this.renderSquare(6)}
+                {this.renderSquare(7)}
+                {this.renderSquare(8)}
             </div>
-            </div>
-       
+          </div> 
         )
       }
 }
